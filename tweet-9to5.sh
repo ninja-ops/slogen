a3063e976d65a674f8de71f597aad9a275d6e598
ninja-ops <bench2600@gmail.com>
2017-09-09 10:29:41 +0200

    tweet sample script

---
+++ tweet-9to5.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+HOUR=`date +%H`
+HOUR=17
+r=$(( $RANDOM % 8 )); 
+
+if [ "$HOUR" -gt 8 ] && [ "$HOUR" -lt 18 ]; then
+  if [ $r -eq 0 ]; then
+    ./slogen.php data/mgt-sprech.txt | twitter set
+  fi
+fi
