f83d93e0c13a1b62025d62bdc839f38257abbdac
ninja-ops <bench2600@gmail.com>
2017-09-11 15:45:10 +0200

    options to shell added

--- tweet-9to5.sh
+++ tweet-9to5.sh
@@ -5,6 +5,6 @@ r=$(( $RANDOM % 8 ));
 
 if [ "$HOUR" -gt 8 ] && [ "$HOUR" -lt 18 ]; then
   if [ $r -eq 0 ]; then
-    ./slogen.php data/mgt-sprech.txt | twitter set
+    ./slogen.php data/mgt-sprech.txt --hashtag --twitterlimit | twitter set
   fi
 fi
