18b03684318341aa3fcdc9333305e025a5fed0db
ninja-ops <bench2600@gmail.com>
2017-09-11 15:44:28 +0200

    --hashtag + --twitterlimit option added

--- slogen.php
+++ slogen.php
@@ -1,8 +1,23 @@
 #!/usr/bin/env php
 <?php
 
-if (!isset($argv[1])) {
+function _usage() {
   echo "usage: slogen.php filename.txt\n\n";
+  echo "options:\n";
+  echo "--hashtag  // add #\n";
+  echo "--twitterlimit // limits to 140 chars\n";
+  echo "\n";
+}
+
+if (
+     (in_array("--help", $argv)) || (in_array("-h", $argv))
+   ) {
+  _usage();
+  exit();
+}
+
+if (!isset($argv[1])) {
+  _usage();
   exit(1);
 } else {
   if (!is_file($argv[1])) {
@@ -22,4 +37,22 @@ foreach($chunks as $chunk) {
   $parts[] = $words[rand(0, count($words)-1)];
 }
 
-echo implode(' ', $parts);
+$output = implode(' ', $parts);
+if (in_array("--twitterlimit", $argv)) {
+  if (strlen($output) > 140) {
+    $output = "";
+  }
+}
+
+if (in_array("--hashtag", $argv)) {
+  $chunks = explode(" ", $output);
+  for ($i=1;$i<count($chunks);$i++) {
+    echo $i . substr($chunks[$i], 0, 1) . "\n";
+    if (substr($chunks[$i], 0, 1) == strtoupper(substr($chunks[$i], 0, 1))) {
+      $chunks[$i] = "#" . $chunks[$i];
+    }
+  }
+  $output = implode(' ', $chunks);
+}
+
+echo $output;
--- slogen.py
+++ slogen.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
-print 'needs implementation'
+import sys
+try:
+  filename = sys.argv[1]
+#except FileNotFoundError:
+#  print 'file not found\n'
+except IndexError:
+  print 'usage: slogen.py filename.txt\n'
+  exit(1)
+except:
+  print("Unexpected error:", sys.exc_info()[0])
+  exit(1)
